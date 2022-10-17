638e004c7b6bd285d3190a444a62af1dbcb7ca20
Michael Maloney <>
2022-10-17 09:03:07 +0000

    Add some more words, check for some duplicates.

--- build.py
+++ build.py
@@ -3,8 +3,13 @@ import json
 
 def main():
     dictionary = {}
+    words = set()
+    canonical_outlines = {}
     for filename in glob('dictionaries/*.json'):
         if filename == 'dictionaries/main.json':
+            continue
+
+        if filename == 'dictionaries/lapwing.json':
             continue
 
         print(filename)
@@ -13,15 +18,21 @@ def main():
                 if outline in dictionary:
                     assert dictionary[outline] == word, f'Words do not match for outline: {word}'
                 dictionary[outline] = word
+                words.add(word)
+                assert word not in canonical_outlines or canonical_outlines[word] == outline, f'Word does not have a canonical outline: {word}'
+                canonical_outlines[word] = outline
 
     with open('seagull.json', 'w') as outfile:
         json.dump(dictionary, outfile, indent=4)
 
     with open('dictionaries/main.json') as infile:
         for outline, word in json.load(infile).items():
-            if outline not in dictionary:
+            if outline not in dictionary and word not in words:
                 dictionary[outline] = f'[Type # for {word!r}]'
                 dictionary[outline + '/#'] = word
+            elif outline not in dictionary: # but word is in words
+                canonical_outline = canonical_outlines[word]
+                dictionary[outline] = f'[Type {canonical_outline} for {word!r}]'
 
     with open('main.bypass.json', 'w') as outfile:
         json.dump(dictionary, outfile, indent=4)
