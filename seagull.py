56d3bad9f00bcf9f4ce0af3a56150fbb9d452ca4
Michael Maloney <>
2022-12-19 03:08:29 +0000

    Add seagull.py.

---
+++ seagull.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import json
+import urllib
+
+
+DICTIONARY_URLS = [
+    'https://raw.githubusercontent.com/tac-tics/seagull/master/seagull.json',
+]
+
+
+DICTIONARY = {}
+
+# Length of the longest supported key (number of strokes).
+LONGEST_KEY = 1
+
+
+def load_dictionary(dictionary_url):
+    global LONGEST_KEY
+    with urllib.request.urlopen(dictionary_url) as infile:
+        dictionary = json.load(infile)
+        for outline, word in dictionary.items():
+            LONGEST_KEY = max(LONGEST_KEY, len(outline.split('/')))
+            DICTIONARY[outline] = word
+
+
+def load_dictionaries():
+    for dictionary_json in reversed(DICTIONARY_URLS):
+        load_dictionary(dictionary_json)
+
+load_dictionaries()
+
+# Lookup function: return the translation for <key> (a tuple of strokes)
+# or raise KeyError if no translation is available/possible.
+def lookup(key):
+    assert len(key) <= LONGEST_KEY
+    outline = '/'.join(key)
+
+    word = DICTIONARY.get(outline)
+    if word is None:
+        raise KeyError
+    else:
+        return word
+
+
+# Optional: return an array of stroke tuples that would translate back
+# to <text> (an empty array if not possible).
+def reverse_lookup(text):
+    return []
